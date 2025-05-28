57b795f19f81aee51bc2334a01cc407a56358ef5
Tomasz Wolicki <tomasz.wolicki@student.put.poznan.pl>
2025-05-28 22:18:13 +0200

    Create disturbed monitor prototype

--- p2/lamport.py
+++ p2/lamport.py
@@ -5,7 +5,7 @@ import time
 import sys
 import json
 
-class DisturbedMonitor():
+class DisturbedMutex():
     def __init__(self):
         self.critical_section_queue = []
         self.device_process_id = "P2"
@@ -17,59 +17,92 @@ class DisturbedMonitor():
         port = 5557
         self.sub_socket.connect(f"tcp://172.17.0.2:{port}")
         self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
-        self.other_processes_count = 2
+        self.other_processes_count = 1
+        self.get_replay = False
+        self.wait = False
+        self.condintion_name = ""
         time.sleep(4)  # Allow some time for the subscriber to connect
-
     
-    def get_message(self,buffer,in_index,out_index):
-        message = self.sub_socket.recv()
-        msg_response = message.decode('utf-8')
-        data = json.loads(msg_response)
-        
-        if data["msg_type"] == "Replay" and data["process"] == self.device_process_id:
-            processid_want_critical_section = self.critical_section_queue[0]
-            if processid_want_critical_section[0] == self.device_process_id:
-                # EnterCriticalSection(self.device_process_id, critical_section_queue, 10, [], 0)
-                self.critical_section_queue.pop(0)
-
-        elif data["msg_type"] == "Request":
-            self.critical_section_queue.append(data["process"],data["time"]) # collections queue.Queue() #moze dodac
-            self.critical_section_queue.sort() #nie wiem w sumie moze to uproscic aby mniejsza zlozonosc obliczeniowa i mnoze zwykle sortowanie bez lambdy jka nie  bedzi edzialac  key=lambda x: x["time"]????
+    def get_message(self,buffer,in_index,out_index):  
+        while True:
+            message = self.sub_socket.recv()
+            msg_response = message.decode('utf-8')
+            data = json.loads(msg_response)
             
-        elif data["msg_type"] == "Release":
-            pass
+            if data["msg_type"] == "Replay" and data["process"] == self.device_process_id:
+                processid_want_critical_section = self.critical_section_queue[0]
+                if processid_want_critical_section[0] == self.device_process_id:
+                    self.critical_section_queue.pop(0)
+                    return buffer, in_index, out_index  # Return the buffer and indices after entering critical section    
+                            
+            elif data["msg_type"] == "Request":
+                self.critical_section_queue.append(data["process"],data["time"]) # collections queue.Queue() #moze dodac
+                self.critical_section_queue.sort() #nie wiem w sumie moze to uproscic aby mniejsza zlozonosc obliczeniowa i mnoze zwykle sortowanie bez lambdy jka nie  bedzi edzialac  key=lambda x: x["time"]????
+                self.replay_message(data["process"])  # Send replay message to the requesting process
 
-        elif data["msg_type"] == "ProcessEndWork":
-            pass
+            elif data["msg_type"] == "Release":
+                
+                buffer = data["buffer"]
+                in_index = data["in_index"]
+                out_index = data["out_index"]
+                
+                if data["Status"] == "wait":
+                    self.critical_section_queue.pop(0)
 
-    def wait(self,buffer,in_index,out_index): #TODO tutaj bym tabele dodal a teraz zmienne
-        #TODO releasemessage
+                    if processid_want_critical_section[0] == self.device_process_id:
+                        self.critical_section_queue.pop(0)
+                        return buffer, in_index, out_index  # Return the buffer and indices after entering critical section   
+                
+                elif data["Status"] == "notify":
+                    if self.wait and data["condition_name"] == self.condintion_name:        
+                        self.wait = False
+                        self.request_message()
+
+            elif data["msg_type"] == "ProcessEndWork":
+                self.other_processes_count -= 1
+                pass
+
+    def wait(self,buffer,in_index,out_index,condition_name): #TODO tutaj bym tabele dodal a teraz zmienne
+        self.condintion_name = condition_name
+        self.release_message("wait",buffer,in_index,out_index)  # Release message with "no" values_changed
         return self.get_message(buffer,in_index,out_index)
-        # while True:
-        #     self.get_message()
-    def notify(self, message):
-        #TODO release_message
-        self.pub_socket.send_string(message)
 
-    def CriticalSectionAllowed(self,buffer,in_index,out_index):
-        self.Request_message()
+    def notify(self, buffer,in_index,out_index):
+        self.release_message("notify", buffer,in_index,out_index)  # Release message with "yes" value
+        return self.get_message(buffer,in_index,out_index)
+
+    def enter_crirical_section(self,buffer,in_index,out_index):
+        self.request_message()
         self.get_message(buffer,in_index,out_index)
-    
-    def Release_message(self):
-        pass
 
-    def Request_message(self):
-        Process = "P2"
-        request_data = {"msg_type": "Request" , "process":  Process, "time": time.time()}
+    def request_message(self):
+        request_data = {"msg_type": "Request" , "process":  self.device_process_id, "time": time.time()}
         message_request = json.dumps(request_data).encode('utf-8') #mozliwe ze nieporzebne
         self.pub_socket.send(message_request) #protocol buffers
     
-    def Replay_message(self,process_id):
-        if process_id == "P2":
-            request_data = {"msg_type": "Replay","Process":  process_id}
-            message_request = json.dumps(request_data).encode('utf-8') #mozliwe ze nieporzebne
-            self.pub_socket.send(message_request) #protocol buffers //TODO rozroznic sockety????
-            
+    def replay_message(self,process_id):
+        request_data = {"msg_type": "Replay","Process":  process_id}
+        message_request = json.dumps(request_data).encode('utf-8') #mozliwe ze nieporzebne
+        self.pub_socket.send(message_request) #protocol buffers //TODO rozroznic sockety????
+
+    def release_message(self,status,buffer,in_index,out_index):
+        
+        if status == "notify":
+            request_data = {"msg_type": "Release","Status": "notify", "condition_name": self.condintion_name, "buffer": buffer, "in_index": in_index, "out_index": out_index}
+        else:
+            request_data = {"msg_type": "Release","Status": "wait", "condition_name": self.condintion_name, "buffer": buffer, "in_index": in_index, "out_index": out_index} #TODO enum  status??
+
+        message_request = json.dumps(request_data).encode('utf-8') #mozliwe ze nieporzebne
+        self.pub_socket.send(message_request) #protocol buffers //TODO rozroznic sockety????
+
+# class DisturbedCondition(DisturbedMutex):
+#     def __init__(self,condition_name):
+#         super().__init__()
+#         self.condition_name = condition_name
+#     # def Wait(self,buffer,in_index,out_index):    
+        
+    
+
 CAPACITY = 10
 buffer = [-1 for _ in range(CAPACITY)]
 in_index = 0
@@ -88,18 +121,17 @@ def Producer(DisturbedMonitor):
         counter += 1
         buffer[in_index] = counter
         print("Producer produced:", counter)
+        print("Buffer state:", buffer)
         in_index = (in_index + 1) % CAPACITY
-
-        DisturbedMonitor.notify(buffer,in_index)  # Signal to consumers
-
-
+        DisturbedMonitor.notify(buffer,in_index)  # Signal to consumers  #TODO tutaj zmienna not_full i full nie musi byc w pelni transparentne btw
+    DisturbedMonitor.EndOfTheWork()
 
 CAPACITY = 10
 buffer = [-1 for _ in range(CAPACITY)]
 in_index = 0
 
-DisturbedMonitor = DisturbedMonitor()
-Producer(DisturbedMonitor,0, [], 0)  # Initialize producer with 0 items produced, empty buffer, and in_index at 0
+DisturbedMutex = DisturbedMutex()
+Producer(DisturbedMutex,0, [], 0)  # Initialize producer with 0 items produced, empty buffer, and in_index at 0
 
 # node_id = "B"
 # peer_ports = ["5556", "5557"]  # np. ["5556", "5557"]
