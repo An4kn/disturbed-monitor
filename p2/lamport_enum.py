e28ade9ddd7c37abcfd8c5c995e69461a4c500f7
Tomasz Wolicki <tomasz.wolicki@student.put.poznan.pl>
2025-06-07 23:27:13 +0200

    Add transparent shared data to process p2

--- p2/lamport_enum.py
+++ p2/lamport_enum.py
@@ -4,6 +4,7 @@ import threading
 import time
 import sys
 import json
+from enum import Enum
 
 class DisturbedMonitor():
     def __init__(self, input_device_process_id,input_all_active_processes, input_pub_socket, input_sub_socket,input_time_sleep): #Dane podstawowe
@@ -20,77 +21,90 @@ class DisturbedMonitor():
         
         self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
         self.replay_from_processes_left = self.all_active_processes.copy()
-        self.get_replay = False
         self.waiting_for_notify = False
         self.condintion_name = ""
         time.sleep(input_time_sleep)
-    
-    def get_message(self,shared_data):  
+
+    def get_message(self,shared_data):
         while True:
+            response_shared_data = {}
+
             message = self.sub_socket.recv()
             msg_response = message.decode('utf-8')
             data = json.loads(msg_response)
             print("queue: ", self.critical_section_queue)
             
-            if data["msg_type"] == "Replay" and data["process"] == self.device_process_id:
-
-                # self.replay_count_needed -= 1
-                self.replay_from_processes_left.discard(data["From_process"])
-                print("Get Replay from process:", data["From_process"], " coutner needed replay from procees: ", self.replay_from_processes_left)
-
-                processid_want_critical_section = self.critical_section_queue[0]
-              
-                if processid_want_critical_section[0] == self.device_process_id and not self.replay_from_processes_left:
-                    self.critical_section_queue.pop(0)
-                    return shared_data  # Return the buffer and indices after entering critical section    
+            if data["msg_type"] == MessageType.REPLAY.value and data["process"] == self.device_process_id:
+                response_shared_data = self.handle_replay_message(data, shared_data)                  
                              
-            elif data["msg_type"] == "Request":
-                print("Get request message from: ",data["process"]," process")
+            elif data["msg_type"] == MessageType.REQUEST.value:
+                response_shared_data = self.handle_request_message(data)
 
-                self.critical_section_queue.append((data["process"],data["time"])) # collections queue.Queue() #moze dodac
-                self.critical_section_queue.sort(key=lambda x: x[1]) #nie wiem w sumie moze to uproscic aby mniejsza zlozonosc obliczeniowa i mnoze zwykle sortowanie bez lambdy jka nie  bedzi edzialac  key=lambda x: x["time"]????
-                # self.replay_count_needed = self.other_processes_count #TODO to moze jest wazne
-                self.replay_message(data["process"])  # Send replay message to the requesting process
+            elif data["msg_type"] == MessageType.RELEASE.value:   
+                response_shared_data = self.handle_release_message(data, shared_data)
+                                            
+            elif data["msg_type"] == MessageType.SHUTDOWN.value:
+                response_shared_data = self.remove_finished_process(shared_data, data)
+            
+            if response_shared_data is not None:
+                return response_shared_data
+            
 
-            elif data["msg_type"] == "Release":
-    
-                shared_data = data["shared_data"]
-                self.critical_section_queue.pop(0)
-                print("Got release message from process:", data["From_process"], "Status: ", data["Status"], " Condition name: ", data["condition_name"], "replay messagesd left: ", self.replay_from_processes_left)
-
-                if data["Status"] == "wait" and len(self.critical_section_queue) > 0:
-                    processid_want_critical_section = self.critical_section_queue[0]
-
-                    if processid_want_critical_section[0] == self.device_process_id:
-                        self.critical_section_queue.pop(0) #TODO czmemu to usunalem nie rozumiem
-                        return shared_data  # Return the buffer and indices after entering critical section   
-                
-                elif data["Status"] == "notify":
-                    if self.waiting_for_notify and data["condition_name"] == self.condintion_name:        
-                        self.waiting_for_notify = False                        
-                        self.request_message()
-
-                    elif not self.waiting_for_notify and not (len(self.critical_section_queue) == 0): #to jeszcze sprawdizc czy tak dziala?
-
-                        processid_want_critical_section = self.critical_section_queue[0]
-                        if processid_want_critical_section[0] == self.device_process_id and not self.replay_from_processes_left:
-                            self.critical_section_queue.pop(0)
-                            return shared_data
-                        
-            elif data["msg_type"] == "ProcessEndWork":
-                print("Get process: ",data["From_process"] , " has ended work, no more requests will be sent.")
-                self.all_active_processes.discard(data["From_process"])
-                self.replay_from_processes_left.discard(data["From_process"])
-                processid_want_critical_section = self.critical_section_queue[0]
+    def remove_finished_process(self, shared_data, data):
+        print("Get process: ",data["From_process"] , " has ended work, no more requests will be sent.")
+        self.all_active_processes.discard(data["From_process"])
+        self.replay_from_processes_left.discard(data["From_process"])
+        processid_want_critical_section = self.critical_section_queue[0]
               
+        if processid_want_critical_section[0] == self.device_process_id and not self.replay_from_processes_left:
+            self.critical_section_queue.pop(0)
+            return shared_data
+        
+    def handle_replay_message(self, data, shared_data):
+        # self.replay_count_needed -= 1
+        self.replay_from_processes_left.discard(data["From_process"])
+        print("Get Replay from process:", data["From_process"], " counter needed replay from processes: ", self.replay_from_processes_left)
+
+        processid_want_critical_section = self.critical_section_queue[0]
+
+        if processid_want_critical_section[0] == self.device_process_id and not self.replay_from_processes_left:
+            self.critical_section_queue.pop(0)
+            return shared_data  # Return the buffer and indices after entering critical section
+        
+    def handle_release_message(self, data, shared_data):
+        shared_data = data["shared_data"]
+        self.critical_section_queue.pop(0)
+        print("Got release message from process:", data["From_process"], "Status: ", data["Status"], " Condition name: ", data["condition_name"], "replay messages left: ", self.replay_from_processes_left)
+
+        if data["Status"] == ReleaseStatus.WAIT.value and len(self.critical_section_queue) > 0:
+            processid_want_critical_section = self.critical_section_queue[0]
+
+            if processid_want_critical_section[0] == self.device_process_id:
+                self.critical_section_queue.pop(0)
+                return shared_data  # Return the buffer and indices after entering critical section   
+
+        elif data["Status"] == ReleaseStatus.NOTIFY.value:
+            if self.waiting_for_notify and data["condition_name"] == self.condintion_name:        
+                self.waiting_for_notify = False                        
+                self.send_request_message()
+
+            elif not self.waiting_for_notify and not (len(self.critical_section_queue) == 0):
+                processid_want_critical_section = self.critical_section_queue[0]
                 if processid_want_critical_section[0] == self.device_process_id and not self.replay_from_processes_left:
                     self.critical_section_queue.pop(0)
                     return shared_data
+        
+    def handle_request_message(self, data):
+        print("Get request message from: ",data["process"]," process")
+        self.critical_section_queue.append((data["process"],data["time"])) # collections queue.Queue() #moze dodac
+        self.critical_section_queue.sort(key=lambda x: x[1]) #nie wiem w sumie moze to uproscic aby mniejsza zlozonosc obliczeniowa i mnoze zwykle sortowanie bez lambdy jka nie  bedzi edzialac  key=lambda x: x["time"]????
+        # self.replay_count_needed = self.other_processes_count #TODO to moze jest wazne
+        self.send_replay_message(data["process"])  # Send replay message to the requesting process                    
 
     def wait(self,shared_data,condition_name): #TODO tutaj bym tabele dodal a teraz zmienne
         self.condintion_name = condition_name
         self.waiting_for_notify = True
-        self.release_message("wait",shared_data)  # Release message with "no" values_changed
+        self.send_release_message(ReleaseStatus.WAIT,shared_data)  # Release message with "no" values_changed
         return self.get_message(shared_data)
 
     def notify(self,shared_data,condition_name):
@@ -99,17 +113,17 @@ class DisturbedMonitor():
         #     return buffer, in_index, out_index
         
         self.condintion_name = condition_name
-        self.release_message("notify", shared_data)  # Release message with "yes" value
+        self.send_release_message(ReleaseStatus.NOTIFY, shared_data)  # Release message with "yes" value
         # return self.get_message(buffer,in_index,out_index)
 
     def acquire_lock(self,shared_data): #aquirelock
         # if self.other_processes_count == 0:
         if not self.all_active_processes:
             return shared_data
-        self.request_message()
+        self.send_request_message()
         return self.get_message(shared_data)
 
-    def request_message(self):
+    def send_request_message(self):
         send_time = time.time()
         # self.replay_count_needed = self.other_processes_count#TODO OOOOO
         self.replay_from_processes_left = self.all_active_processes.copy()
@@ -117,23 +131,23 @@ class DisturbedMonitor():
         print("Requesting critical section at time:", send_time)
 
         self.critical_section_queue.append((self.device_process_id ,send_time)) 
-        request_data = {"msg_type": "Request" , "process":  self.device_process_id, "time": time.time()}
+        request_data = {"msg_type":MessageType.REQUEST , "process":  self.device_process_id, "time": time.time()}
 
         message_request = json.dumps(request_data).encode('utf-8') #mozliwe ze nieporzebne
         self.pub_socket.send(message_request) #protocol buffers
     
-    def replay_message(self,process_id):
-        request_data = {"msg_type": "Replay","process":  process_id, "From_process": self.device_process_id}
+    def send_replay_message(self,replay_process_id):
+        request_data = {"msg_type": MessageType.REPLAY,"process":  replay_process_id, "From_process": self.device_process_id}
         message_request = json.dumps(request_data).encode('utf-8') #mozliwe ze nieporzebne
-        print("Sending replay message to process:", process_id)
+        print("Sending replay message to process:", replay_process_id)
 
         self.pub_socket.send(message_request) #protocol buffers //TODO rozroznic sockety????
 
-    def release_message(self,status,shared_data):        
-        if status == "notify":
-            request_data = {"msg_type": "Release","Status": "notify", "condition_name": self.condintion_name, "shared_data": shared_data, "From_process": self.device_process_id} #TODO enum  status??
+    def send_release_message(self,status,shared_data):        
+        if status == ReleaseStatus.NOTIFY:
+            request_data = {"msg_type": MessageType.RELEASE ,"Status": ReleaseStatus.NOTIFY, "condition_name": self.condintion_name, "shared_data": shared_data, "From_process": self.device_process_id} #TODO enum  status??
         else:
-            request_data = {"msg_type": "Release","Status": "wait", "condition_name": self.condintion_name, "shared_data": shared_data,"From_process": self.device_process_id } #TODO enum  status??
+            request_data = {"msg_type": MessageType.RELEASE,"Status": ReleaseStatus.WAIT, "condition_name": self.condintion_name, "shared_data": shared_data,"From_process": self.device_process_id } #TODO enum  status??
 
         print("Sending release message with status:", status, "to all processes, condition name:", self.condintion_name)
 
@@ -141,12 +155,22 @@ class DisturbedMonitor():
         self.pub_socket.send(message_request) #protocol buffers //TODO rozroznic sockety????
     
     def join(self):
-        request_data = {"msg_type": "ProcessEndWork", "From_process": self.device_process_id} #TODO form proces usunac
+        request_data = {"msg_type": MessageType.SHUTDOWN, "From_process": self.device_process_id} #TODO form proces usunac
         message_request = json.dumps(request_data).encode('utf-8')
         self.pub_socket.send(message_request)
-        print("Process has ended work, no more requests will be sent.")           
+        print("Process has ended work, no more requests will be sent.")
+    
+class MessageType(str, Enum):
+    """Definiuje typy wiadomości w protokole komunikacyjnym."""
+    REQUEST = "Request"
+    REPLAY = "Replay"
+    RELEASE = "Release"
+    SHUTDOWN = "Shutdown"
 
-
+class ReleaseStatus(str, Enum):
+    """Definiuje statusy w wiadomości zwalniającej blokadę."""
+    WAIT = "wait"
+    NOTIFY = "notify"
 def serialize_shared_data(buffer, in_index,out_index):
   return {
       "buffer": buffer,
